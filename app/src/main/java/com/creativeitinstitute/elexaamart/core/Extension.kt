74de052a87a2d13af4ca71f31ea64facd5beee6c
jubo445 <juboraj335.ja@gmail.com>
2025-04-10 19:59:13 +0600

    runtime permission added

---
+++ app/src/main/java/com/creativeitinstitute/elexaamart/core/Extension.kt
@@ -0,0 +1,31 @@
+@file:Suppress("DEPRECATION")
+
+package com.creativeitinstitute.elexaamart.core
+
+
+import android.content.pm.PackageManager
+import android.widget.EditText
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+
+
+fun EditText.extract() : String{
+
+  return  text.toString().trim()
+}
+
+fun Fragment.requestPermission(
+  request: ActivityResultLauncher<Array<String>>,
+  permission: Array<String>
+){
+ request.launch(permission)
+}
+
+fun Fragment.areAllPermissionsGranted(permissions: Array<String>): Boolean{
+
+  return permissions.all {
+    ContextCompat.checkSelfPermission(requireContext(),it) == PackageManager.PERMISSION_GRANTED
+  }
+}
--- app/src/main/java/com/creativeitinstitute/elexaamart/views/dashboard/seller/upload/UploadProductFragment.kt
+++ app/src/main/java/com/creativeitinstitute/elexaamart/views/dashboard/seller/upload/UploadProductFragment.kt
@@ -1,21 +1,86 @@
 package com.creativeitinstitute.elexaamart.views.dashboard.seller.upload
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.creativeitinstitute.elexaamart.R
 
-class UploadProductFragment : Fragment() {
+import android.Manifest
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_product, container, false)
+import com.creativeitinstitute.elexaamart.base.BaseFragment
+import com.creativeitinstitute.elexaamart.core.areAllPermissionsGranted
+import com.creativeitinstitute.elexaamart.core.extract
+import com.creativeitinstitute.elexaamart.core.requestPermission
+import com.creativeitinstitute.elexaamart.data.Product
+import com.creativeitinstitute.elexaamart.databinding.FragmentUploadProductBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class UploadProductFragment : BaseFragment<FragmentUploadProductBinding>(
+    FragmentUploadProductBinding::inflate
+) {
+    override fun setListener() {
+
+        permissionsRequest = getPermissionRequest()
+
+
+        binding.apply {
+            ivProduct.setOnClickListener {
+
+                requestPermission(permissionsRequest, permissionList)
+            }
+
+            btnUploadProduct.setOnClickListener {
+
+                val name = etProductName.extract()
+                val price = etProductPrice.extract()
+                val description = etProductDescription.extract()
+                val amount = etProductAmount.extract()
+
+                val product = Product(
+                    name = name,
+                    description = description,
+                    price = price.toDouble(),
+                    amount = amount.toInt()
+                )
+
+                uploadProduct(product)
+            }
+        }
+
+
     }
+
+    private fun getPermissionRequest(): ActivityResultLauncher<Array<String>> {
+
+        return registerForActivityResult (ActivityResultContracts.RequestMultiplePermissions()){
+
+            if (areAllPermissionsGranted(permissionList)){
+
+                Toast.makeText(requireContext(), "Granted", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(requireContext(), "Not Granted", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun uploadProduct(product: Product) {
+
+    }
+
+    override fun allObserver() {
+
+    }
+
+    companion object{
+        private val permissionList = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        )
+    }
+
+    private lateinit var permissionsRequest: ActivityResultLauncher<Array<String>>
 
 
 }
